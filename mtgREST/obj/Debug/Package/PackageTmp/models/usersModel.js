621dede08a1bb0a6932e4d5a574d5c0a02279ff9
tanisman <mail@sezercantanisman.com.tr>
2017-12-15 22:10:05 +0200

    firstcommit

---
+++ mtgREST/obj/Debug/Package/PackageTmp/models/usersModel.js
@@ -0,0 +1,29 @@
+﻿'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var UserSchema = new Schema({
+    username: {
+        type: String,
+        required: 'Username'
+    },
+    password: {
+        type: String,
+        required: 'Password'
+    },
+    register_date: {
+        type: Date,
+        default: Date.now
+    },
+    email: {
+        type: String,
+        required: "E-Mail Address"
+    },
+    phone_num: {
+        type: String,
+        required: "Telephone Num."
+    }
+});
+
+module.exports = mongoose.model('Users', UserSchema);
---
+++ mtgREST/obj/Debug/Package/PackageTmp/routes/users.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+var usersController = require('../controllers/usersController');
+
+/* GET users listing. */
+router.get('/', usersController.list_all_users);
+router.post('/', usersController.create_a_user);
+router.get('/:userId', usersController.read_a_user);
+
+module.exports = router;
